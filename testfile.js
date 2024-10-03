6b9fdd5fb9e08ec2da75da7be1f302a13058a6f9
M.Bilal Khan <bilalkhan.192003@gmail.com>
2024-10-03 10:33:00 +0500

    Update testfile.js

--- testfile.js
+++ testfile.js
@@ -1,1 +1,18 @@
-console.log("hi");
+#!/bin/bash
+#echo "server configration"
+#list_of_files_in_current_dir=$(pwd | ls)
+#echo "$list_of_files_in_current_dir"
+#if [ -d "test" ]
+#then 
+#	echo "test dir exist"
+#else
+#	echo "test dir does not exist"
+#	mkdir "test"
+#fi
+#param=$1
+#echo "$1"
+
+#Input
+read -p "Enter Your Name" user_name
+echo "Your Name is $user_name"
+
